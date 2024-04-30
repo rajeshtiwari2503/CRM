021a9461f80af52e07dd9cb5c22cb3b15b187b10
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-04-30 09:44:21 +0530

    crm first

---
+++ src/app/page.js
@@ -0,0 +1,21 @@
+ 
+import SignIn from "./sign_in/page";
+import Sidenav from "./components/Sidenav";
+import Dashboard from "./dashboard/page";
+ 
+ 
+
+ 
+ 
+ 
+
+export default function Home() {
+  return (
+    <main className="   ">
+      {/* <SignIn /> */}
+       
+     <Dashboard />
+    
+    </main>
+  )
+}
